4a420f43d8bf002b658915e5db390bf106fb0345
Lisa <lisa.epe95@web.de>
2022-05-15 16:23:31 +0200

    Update and parasite families

--- R/CommunityAnalyses.R
+++ R/CommunityAnalyses.R
@@ -3,11 +3,12 @@
 ## 
 #####################
 
+
 library(boral)
 library(data.table)
 library(ggplot2)
 library(corrplot)
-library(rethinking)  
+#library(rethinking)  
 
 rm(list=ls(all=TRUE))
 
@@ -25,11 +26,12 @@ LOS = function(x){
 # Put the data together ---------------------------------------------------
 
 # Fit treatments
+setwd("C:/Users/Lisa/Desktop/FoM AG Kurtz/Analysis/Lisa/")
 Data = read.csv("Data/Rotifers.csv")
 
-Data$Exposed = factor(Data$Exposed)
-levels(Data$Exposed)
-levels(Data$Exposed) = c("C", "E+")
+#Data$Exposed = factor(Data$Exposed)
+#levels(Data$Exposed)
+#levels(Data$Exposed) = c("C", "E+")
 
 Data$infection = factor(Data$infection)
 levels(Data$infection)
@@ -37,7 +39,7 @@ levels(Data$infection)
 Data$Exposure = factor(Data$infection)
 levels(Data$Exposure) = c("C", "E", "E", "E", "E")
 levels(Data$Exposure)
-Data$Infection = factor(Data$Exposed:Data$Infected)
+Data$Infection = factor(Data$Exposed:Data$Infected) #?
 
 head(Data)
 
@@ -88,7 +90,7 @@ rescors0 <- get.residual.cor(fit0, est = "median")
 # Often used in other areas of multivariate statistics, 
 # the trace may be interpreted as
 # the amount of covariation explained by the latent variables. 
-(rescors0$trace/rescors1$trace-1) *100# 
+(rescors0$trace/rescors2$trace-1) *100# 
 
 
 # Correlation plots -------------------------------------------------------
@@ -102,7 +104,7 @@ corrplot(rescors2$sig.cor, type = "lower", diag = FALSE,
          tl.srt = 45)
 
 
-corrplot(Testcors1$sig.cor, type = "lower", diag = FALSE,
+corrplot(treatcors2$sig.cor, type = "lower", diag = FALSE,
          title = "Correlations due to covariates",  mar = c(3,0.5,2,1),
          tl.srt = 45)
 
@@ -116,31 +118,113 @@ varP = data.frame(varP$varpart.X)
 varP$Factor = c("B0",names(data.frame(X1)))
 varP = varP[-1,]
 
-df_plot = melt(varP, value.name = "Factor")
+df_plot = reshape2::melt(varP, value.name = "Factor")
 
-names(df_plot)[2:3] = c("Cluster", "RI")
+names(df_plot)[2:3] = c("Species", "RI")
 df_plot$Factor = factor(df_plot$Factor)
 
 levels(df_plot$Factor)
-levels(df_plot$Factor) <- c("Expose","Exposed:Time", "Time")
+levels(df_plot$Factor) <- c("Exposed","Exposed:Time", "Time")
 levels(df_plot$Factor)
-df_plot$Factor = factor(df_plot$Factor, levels = c( "Time", "Expose","Exposed:Time"))
+df_plot$Factor = factor(df_plot$Factor, levels = c( "Time", "Exposed","Exposed:Time"))
 df_plot$RI = df_plot$RI *100
-df_plot$Cluster = factor(df_plot$Cluster)
+df_plot$Species = factor(df_plot$Species)
 
 
 
-marc_plot = ggplot(df_plot, aes(x = Cluster, y = RI, fill = Factor)) +
+lisa_plot = ggplot(df_plot, aes(x = Species, y = RI, fill = Factor)) +
   geom_bar(stat = "identity", colour="black") + theme_bw() + theme(panel.grid.major = element_blank())
 
-marc_plot
+lisa_plot
 
 # saves the plots
-png("Plots/RI_rotifs.png")
-marc_plot+scale_fill_brewer(palette="Set2") + ylab("Relative importance (%)") + ylim(c(0,100))
+png("Plots/RI_rotifers.png")
+lisa_plot+scale_fill_brewer(palette="Set2") + ylab("Relative importance (%)") + ylim(c(0,100))
 graphics.off()
 #
 
+
+
+##################################################
+### considering individual parasite families #####
+##################################################
+
+
+head(Data)
+X1 = model.matrix(~ infection * time.point, Data)[,-1]
+testpath3 <- file.path(tempdir(), "jagsboralmodel3.txt")
+fit3 = boral(y= Y, X =X1,  family = "negative.binomial", 
+             lv.control = list(num.lv = 2), save.model = TRUE, 
+             mcmc.control = example_mcmc_control, model.name = testpath3, 
+             ssvs.index = 1, calc.ics = T, ssvs.index = 1 )
+
+
+
+treatcors3 <- get.enviro.cor(fit3, est = "median")
+rescors3 <- get.residual.cor(fit3, est = "median")
+rescors0 <- get.residual.cor(fit0, est = "median")
+
+
+# Often used in other areas of multivariate statistics, 
+# the trace may be interpreted as
+# the amount of covariation explained by the latent variables. 
+(rescors0$trace/rescors3$trace-1) *100# 
+
+
+# Correlation plots -------------------------------------------------------
+
+corrplot(rescors0$sig.cor, type = "lower", diag = FALSE,
+         title = " Residual Correlations",  mar = c(3,0.5,2,1),
+         tl.srt = 45)
+
+corrplot(rescors3$sig.cor, type = "lower", diag = FALSE,
+         title = " Residual Correlations",  mar = c(3,0.5,2,1),
+         tl.srt = 45)
+
+
+corrplot(treatcors3$sig.cor, type = "lower", diag = FALSE,
+         title = "Correlations due to covariates",  mar = c(3,0.5,2,1),
+         tl.srt = 45)
+
+
+## Calcuate the explain variance from the treatments
+
+varP = calc.varpart(fit3, groupX = 1:(fit3$num.X+1)  )
+varP
+
+varP = data.frame(varP$varpart.X)
+varP$Factor = c("B0",names(data.frame(X1)))
+varP = varP[-1,]
+
+df_plot = reshape2::melt(varP, value.name = "Factor")
+
+names(df_plot)[2:3] = c("Species", "RI")
+df_plot$Factor = factor(df_plot$Factor)
+
+levels(df_plot$Factor)
+levels(df_plot$Factor) <- c("IBB.2","IBB.2:Time","IBB.4","IBB.4:Time","LK.1","LK.1:Time","LK.2","LK.2:Time", "Time")
+levels(df_plot$Factor)
+df_plot$Factor = factor(df_plot$Factor, levels = c( "IBB.2","IBB.2:Time","IBB.4","IBB.4:Time","LK.1","LK.1:Time","LK.2","LK.2:Time", "Time"))
+df_plot$RI = df_plot$RI *100
+df_plot$Species = factor(df_plot$Species)
+
+
+
+lisa_plot = ggplot(df_plot, aes(x = Species, y = RI, fill = Factor)) +
+  geom_bar(stat = "identity", colour="black") + theme_bw() + theme(panel.grid.major = element_blank())
+
+lisa_plot
+
+# saves the plots
+png("Plots/RI_rotiferfam2.png")
+lisa_plot+ ylab("Relative importance (%)") + ylim(c(0,100))
+graphics.off()
+#
+
+
+
+
+######################
 svg("RI_Overallcells.svg", width = 6, height = 5)
 ggplot(df_plot, aes(x = Factor, y = RI, fill = Factor)) +
   geom_boxplot() + theme_bw() + theme(panel.grid.major = element_blank()) + 
